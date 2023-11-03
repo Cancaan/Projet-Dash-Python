b12ecbb2b675c83d93e0b1d0db47b347fea7cc48
Cancaan <103224447+Cancaan@users.noreply.github.com>
2023-11-03 16:47:12 +0100

    Update layout.py

--- dashboard/layout.py
+++ dashboard/layout.py
@@ -14,6 +14,7 @@ def sdg_map(df):
     fig.update_layout(width=1280, height=720, title_x=0.5)
     return fig
 
+
 def layout(df):
     return  html.Div([
         html.H1("Sustainable Development Dashboard"),
@@ -31,8 +32,10 @@ def layout(df):
             ),
             dcc.Graph(id='bar-chart',
                     figure={
-                        'data': [{'x': df[df['country'] == "Afghanistan"]['year'], 'y': df[df['country'] == "Afghanistan"]['sdg_index_score'], 'type': 'bar', 'name': 'Index score'},
-                        ],
+                        'data': [{'x': df[df['country'] == "Afghanistan"]
+                                  ['year'], 'y': df[df['country'] == "Afghanistan"]
+                                  ['sdg_index_score'], 'type': 'bar', 'name': 'Index score'},
+                                ],
                         'layout': {
                             'title': f'Index score of Afghanistan',
                             'xaxis': {'title': 'Year'},
@@ -40,3 +43,12 @@ def layout(df):
             })
         ])
     ])
+
+
+def correlation_poverty_hunger(df):
+    selected_columns = ['country', 'year', 'No_Poverty', 'No_Hunger']
+    data_subset = df[selected_columns]
+    fig = px.scatter(data_subset, x='No_Poverty', y='No_Hunger', 
+                     text='country', title='Relation Beetween Poverty and Hunger')
+    fig.update_layout(width=1280, height=720, title_x=0.5)
+    return fig
